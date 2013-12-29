e15a026fd600552a053ae3c82630542b62320d7b
Carol Skelly <carol@iatek.com>
2013-12-29 10:25:04 -0500

    fix json

--- templates.js
+++ templates.js
@@ -12,5 +12,57 @@ module.exports =
         sourceUrl:"http://bootply.com/101004",
         previewUrl:"http://bootply.com/render/101004",
         author:"Bootply.com"
+    },
+    {
+        id:"100993",
+        name:"Sidebar",
+        title:"Sidebar Template",
+        short:"Popular left column layout",
+        full:"This is a popular 2 column template for Bootstrap 3 that includes a left-side 33% width column, and 66% width content area.",
+        tags:["sidebar","starter"],
+        imgUrl:"/assets/templates/100993.png",
+        downloadUrl:"http://bootply.com/download/100993",
+        sourceUrl:"http://bootply.com/100993",
+        previewUrl:"http://bootply.com/render/100993",
+        author:"Bootply.com"
+    },
+    {
+        id:"94967",
+        name:"Fixed_Width",
+        title:"Fixed Width Template",
+        short:"Pixel-based container layout",
+        full:"Standard Bootstrap uses a fluid or percentage-based container. This template exemplifies a fixed or pixel-based container.",
+        tags:["starter","fixed","width"],
+        imgUrl:"/assets/templates/94967.png",
+        downloadUrl:"http://bootply.com/download/94967",
+        sourceUrl:"http://bootply.com/94967",
+        previewUrl:"http://bootply.com/render/94967",
+        author:"Bootply.com"
+    },
+	{
+        id:"94967",
+        name:"Fixed Width Template - /assets/templates/94967.png",
+        title:"Fixed Width Template",
+        short:"Pixel-based container layout",
+        full:"Standard Bootstrap uses a fluid or percentage-based container. This template exemplifies a fixed or pixel-based container.",
+        tags:["starter","fixed","width"],
+        imgUrl:"/assets/templates/94967.png",
+        downloadUrl:"http://bootply.com/download/94967",
+        sourceUrl:"http://bootply.com/94967",
+        previewUrl:"http://bootply.com/render/94967",
+        author:"Bootply.com"
+    },
+	{
+        id:"75225",
+        name:"Fixed Navbar - /assets/templates/75225.png",
+        title:"Fixed Width Template",
+        short:"Pixel-based container layout",
+        full:"Standard Bootstrap uses a fluid or percentage-based container. This template exemplifies a fixed or pixel-based container.",
+        tags:["starter","fixed","width"],
+        imgUrl:"/assets/templates/75225.png",
+        downloadUrl:"http://bootply.com/download/75225",
+        sourceUrl:"http://bootply.com/75225",
+        previewUrl:"http://bootply.com/render/75225",
+        author:"Bootply.com"
     }
 ]
