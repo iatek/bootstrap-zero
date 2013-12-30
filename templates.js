5ee8a1be767763aa47058881e1e8160bdcf3d758
Carol Skelly <carol@iatek.com>
2013-12-30 07:56:20 -0500

    fix json

--- templates.js
+++ templates.js
@@ -83,7 +83,7 @@ module.exports =
         name:"Affix Sidebar - /assets/templates/100983.png",
         title:"Affix Sidebar",
         short:"Sidebar like the Bootstrap docs",
-        full:"Similar to the Bootstrap documentation, this template features a left-side nav that attaches once scrolling goes beyond a large top header.",
+        full:"Similar to the Bootstrap documentation, this template features a left-side nav that attaches once scrolling goes beyond a large top header with ad space.",
         tags:["starter","fixed","affix","sidebar","pin"],
         imgUrl:"http://www.bootply.com/assets/templates/100983.png",
         downloadUrl:"http://bootply.com/download/100983",
