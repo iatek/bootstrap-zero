93dd7c59e357d955821eff79b7159fa562182802
Carol Skelly <carol@iatek.com>
2014-03-19 08:31:52 -0400

    new template

--- templates.js
+++ templates.js
@@ -1004,5 +1004,22 @@ module.exports =
         avatar:"http://www.gettemplate.com/content/images/gt.png",
         license:"CC Attribution",
         licenseUrl:"http://creativecommons.org/licenses/by/3.0/"
+    },
+    {
+        name:"AdminLTE",
+        title:"AdminLTE",
+        short:"Admin dashboard template",
+        full:"A free premium admin dashboard and control panel theme by Almsaeed Studio.",
+        tags:["admin", "theme", "dashboard", "control-panel"],
+        imgUrl:"http://www.daveismyname.com/system/ckeditor/elFinder/files/images/blog/design/adminlte-dashboard.png",
+        downloadUrl:"http://almsaeedstudio.com/download/AdminLTE-master",
+        sourceUrl:"https://github.com/almasaeed2010/AdminLTE/",
+        previewUrl:"http://almsaeedstudio.com/preview",
+        author:"Almsaeed Studio",
+        authorUrl:"http://almsaeedstudio.com/",
+        bootstrapVersion:"3.0.3",
+        avatar:"https://pbs.twimg.com/profile_images/378800000050649381/41e2b37e68cdb7b6ace31ef6be3f093d.jpeg",
+        license:"MIT",
+        licenseUrl:""
     }
 ]
